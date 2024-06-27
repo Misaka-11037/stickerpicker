8400857d00b700e87ac6d3666f9cb2fb81eb923f
Misaka-11037 <i@misaka.me>
2024-06-27 19:49:51 +0800

    add pack Exusiai

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'fa52fe36'
+version = '0.1.0+dev.fa52fe36'
+linkified_version = '0.1.0+dev.[fa52fe36](https://github.com/maunium/stickerpicker/commit/fa52fe363e1fd1069dadfbafcb4cd25b5b61e56f)'
